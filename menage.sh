589e447c4384ebe2564fd45247e39fb81f50d4df
Florian <florian.a@live.fr>
2018-12-18 15:36:57 +0100

    Script tri

--- menage.sh
+++ menage.sh
@@ -1,20 +1,39 @@
-echo "Nettpyage"
-du -a bazar
+echo "Nettoyage"
 
-mkdir bazar_clean
-mkdir bazar_clean/videos
-mkdir bazar_clean/images
-mkdir bazar_clean/divers
+CURRENT_DIRECTORY=`dirname $0`
+CLEANDIRECTORY="$CURRENT_DIRECTORY/bazar_clean"
+rm -rf $CLEANDIRECTORY
+mkdir $CLEANDIRECTORY
 
-find bazar/ -name "*.mp4" -exec cp {} bazar_clean/videos \;
-
-find bazar/ -name "*.png" -exec cp {} bazar_clean/images \;
+mkdir -p $CLEANDIRECTORY/images $CLEANDIRECTORY/videos $CLEANDIRECTORY/divers
 
 FILES=`find bazar/ -type f`
 
 for FILE in $FILES
 do
 
-echo FILE
+file $FILE | grep 'image data' > /dev/null
+IS_IMAGE=$?
+
+file $FILE | grep 'ISO Media' > /dev/null
+IS_VIDEO=$?
+file $FILE | grep 'Matroska data' > /dev/null
+IS_VIDEO=$?
+
+if [ $IS_IMAGE == 0 ]
+then
+MONTH=`date -r $FILE "+%m"`
+YEAR=`date -r $FILE "+%Y"`
+mkdir -p $CLEANDIRECTORY/images/$YEAR/$MONTH
+cp $FILE bazar_clean/images/$YEAR/$MONTH
+
+elif [ $IS_VIDEO == 0 ]
+then
+cp $FILE bazar_clean/videos
+
+else
+cp $FILE bazar_clean/divers
+
+fi
 
 done
